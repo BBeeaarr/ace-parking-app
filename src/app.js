e332f51ed98c970861d0621dfe82ba9b47e02cd4
Ourmond <armandrv@uci.edu>
2026-01-09 23:49:03 -0800

    hello

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { notFoundHandler, errorHandler } = require("./middleware/errorHandler");
 
+const publicRoutes = require("./routes/public.routes");
 
 function createApp() {
   const app = express();
 
   app.use(express.json({ limit: "1mb" }));
+
+	app.use("/api/public", publicRoutes);
 
   // 404 + error middleware
   app.use(notFoundHandler);
