68fc71b8f33908b74ed77f9b7b166749863e8072
Ourmond <armandrv@uci.edu>
2026-04-27 00:20:25 -0700

    add post

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -8,4 +8,10 @@ router.get("/hello", (req, res) => {
   res.json({ message: req.body || "Hello, world!" });
 });
 
+router.post("/hello", (req, res) => {
+  console.log("Hello endpoint called");
+  console.log(req.body);
+  res.json({ message: req.body || "Hello, world!" });
+});
+
 module.exports = router;
